c2f15fb713b838d361f4a7dfab1aa11c451f41e1
mariekwo <marie4@hawaii.edu>
2026-04-11 21:02:04 -1000

    removed tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const metadata: Metadata = {
   title: "Study Viser",
@@ -12,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body>
+       {children}
+      </body>
     </html>
   );
 }
