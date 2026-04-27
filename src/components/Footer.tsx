20deee7205ed574744f602fa13575ddfa1432ce4
Noah Asano <pili.asano@gmail.com>
2026-04-27 10:54:16 -1000

    Add playwright tests

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => (
       <Row className="justify-content-center text-center">
         <Col>
           <p className="footer-university">
-            University <em>of</em> Hawai&#x02BB;i&#174; <em>at </em> M&#x101;noa
+            University of Hawai&#x02BB;i at M&#x101;noa
           </p>
           <p className="footer-subtitle">
             Ke Kulanui o Hawai&#x02BB;i ma M&#x101;noa
---
+++ tests/auth/register.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('user can register as student', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.locator('div').filter({ hasText: /^Student$/ }).nth(1).click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('Stewart Dents');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('stewdent@foo.com');
+  await page.getByRole('textbox', { name: 'Password', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Password', exact: true }).fill('changeme');
+  await page.getByRole('textbox', { name: 'Confirm Password' }).click();
+  await page.getByRole('textbox', { name: 'Confirm Password' }).fill('changeme');
+  await page.getByRole('button', { name: 'Register' }).click();
+});
+
+test('user can register as instructor', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.locator('div').filter({ hasText: /^Instructor$/ }).nth(1).click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('Tee Char');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('teechar@foo.com');
+  await page.getByRole('textbox', { name: 'Password', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Password', exact: true }).fill('changeme');
+  await page.getByRole('textbox', { name: 'Confirm Password' }).click();
+  await page.getByRole('textbox', { name: 'Confirm Password' }).fill('changeme');
+  await page.getByRole('button', { name: 'Register' }).click();
+});
